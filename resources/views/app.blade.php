00006dadad3985b594f7352e04f26a739dcd3d0e
Óscar <oscarvegaherrera59@gmail.com>
2018-10-20 16:51:08 +0200

    Carga inicial

---
+++ resources/views/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="{{url('css/custom.css')}}">
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        @include('navbar')
+
+        <div class="container">
+            
+            @yield('content')
+
+            
+        </div>
+
+        @include('footer')
+        
+    </body>
+</html>
---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+@section('content')
+	
+	<div id="homepage">
+		<div class="panel panel-primary">
+		<div class="panel-heading">
+			Texto
+		</div>
+
+		<div class="panel-body">
+			Cuerpo
+		</div>
+	</div>
+	</div>
+@endsection
